f5ee3f95d2a8609386c62e243e62cee8bc03aef2
J-hana01 <hyelym_2@naver.com>
2026-06-22 01:08:15 +0000

    Create Calendar Screen

---
+++ frontend/calendar_screen.dart
@@ -0,0 +1,16 @@
+// StudyFlow Calendar Screen
+
+class CalendarScreen {
+  void showMonthlySchedule() {
+    print("=== 2026년 6월 ===");
+
+    print("22일");
+    print("09:00 Subject001 V3");
+    print("14:00 Monitor Visit");
+
+    print("");
+
+    print("23일");
+    print("10:00 Subject002 Screening");
+  }
+}
