ad2a15fa2a508afbcfd7015e840f6a00a78c4f78
J-hana01 <hyelym_2@naver.com>
2026-06-22 01:09:13 +0000

    Create Add Schedule Screen

---
+++ frontend/add_schedule_screen.dart
@@ -0,0 +1,16 @@
+// StudyFlow Add Schedule Screen
+
+class AddScheduleScreen {
+  void addSchedule() {
+
+    print("=== 새 일정 등록 ===");
+
+    print("Study Name : ABC-101");
+    print("Subject Number : 001");
+    print("Visit : V3");
+    print("Date : 2026-06-22");
+    print("Time : 09:00");
+    print("Memo : 채혈 + ECG");
+
+  }
+}
