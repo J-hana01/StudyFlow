45ada1da10a057085b6478d293d58aa2f543d1f1
J-hana01 <hyelym_2@naver.com>
2026-06-22 01:06:43 +0000

    Create Home Screen

---
+++ frontend/home_screen.dart
@@ -0,0 +1,16 @@
+// StudyFlow Home Screen
+
+class HomeScreen {
+  void showTodaySchedule() {
+    print("=== 오늘 일정 ===");
+
+    print("09:00 Subject001 V3");
+    print("13:00 Monitor Visit");
+
+    print("");
+    print("=== 다가오는 일정 ===");
+
+    print("D-1 지속심의 제출");
+    print("D-3 Subject002 전화 추적");
+  }
+}
